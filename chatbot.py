ee168a54c6b2c8e0b713329091657e22211a019f
jeff nogueira <36955545+jeff-nogueira@users.noreply.github.com>
2018-03-25 11:59:05 -0300

    Chatterbot
    bot desenvolvido com o modulo chatterbot

---
+++ chatbot.py
@@ -0,0 +1,16 @@
+# -*- Coding: utf-8 -*-
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+
+bot = ChatBot('Test')
+
+conv = ['oi','olá','tudo bem','Sim,e voce','bem','que bom']
+
+bot.set_trainer(ListTrainer)
+bot.train(conv)
+
+while True:
+    quest = input('Voce:')
+
+    response = bot.get_response(quest)
+    print ('Bot:', response)
