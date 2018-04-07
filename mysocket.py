0b296acc1ab87460e7fdf166cd5ffdd55954ecc6
jeff nogueira <36955545+jeff-nogueira@users.noreply.github.com>
2018-04-06 22:58:06 -0300

    Update mysocket.py

--- mysocket.py
+++ mysocket.py
@@ -1,13 +1,13 @@
 import socket
 
-targert_host = "google.com"
+targert_host = "host"
 target_port = 80
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target_host,target_port))
 
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send("GET / HTTP/1.1\r\nHost:host.com\r\n\r\n")
 response = client.recv(4096)
 
 print response
